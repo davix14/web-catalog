1e372c848a34150a93ff1bad3764896ac881bc8f
David <jdavidjuarez95@gmail.com>
2020-03-21 01:57:21 -0400

    First run of webpage and server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");//requre express
 var app = express ();//instantiate express
 
 //Enter server logic here
+app.use(express.static("public"));
 
 //Start web server on port 3010
 app.listen(3010);
