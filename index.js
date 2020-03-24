bb2e4e358efbe53374c006d19304b28f82302b36
David <jdavidjuarez95@gmail.com>
2020-03-23 23:11:38 -0400

    Added event listener as practice

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require("express");//requre express
 var app = express ();//instantiate express
+var bodyParser = require("body-parser");
 
 //Enter server logic here
 app.use(express.static("public"));
+
+//Add logic to handle form input and display
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res){
+	res.sendFile('catalog.html');
+});
+
+app.post("/catalog.html", function(req, res){
+	var title = req.body.movie_Title;
+	var rating = req.body.Rating;
+	res.send("<h1>Title: " + title + " Rating: " + rating + "</h1>");
+});
 
 //Start web server on port 3010
 app.listen(3010);
--- public/app_Main.js
+++ public/app_Main.js
@@ -1,6 +1,11 @@
 //Take list of hotels and add to a visible list
 function addListItem(){
-	var list = document.getElementById('exisitng_list');
-	list.addNew
-	
+	var list;
+	list = document.getElementById('Extg_li2');
+	list.className = 'good';
+
 }
+
+var link;
+link = document.getElementById('Extg_li2');
+link.addEventListener('click', addListItem, false);
