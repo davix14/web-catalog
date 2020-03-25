b51857397d8da5e32cab4002a2d59cac79b6548a
David <jdavidjuarez95@gmail.com>
2020-03-24 23:27:34 -0400

    New listener and function to add new entries

--- public/app_Main.js
+++ public/app_Main.js
@@ -31,11 +31,51 @@ submitBtn.addEventListener('click', function(e){
 	e.preventDefault(); //prevent default action (aka submitting form)
 	//get values of form
 	var newTitle = document.getElementById('new-movie-title');
-	var newRaing = document.getElementById('new-movie-rating');
+	var newRating = document.getElementById('new-movie-rating');
 
 	console.log(newTitle.value);
-	console.log(newRaing.value);
+	console.log(newRating.value);
 
+	//create new elements with id's
+	var newDiv = document.createElement("div");
+	newDiv.setAttribute("id", "existing-movie2");
+
+	var newUl = document.createElement("ul");
+	newUl.setAttribute("id", "movie2-list");
+
+	var newLiTitle = document.createElement("li");
+	newLiTitle.setAttribute("id", "movie2-title");
+	var newTitleText = document.createTextNode(newTitle.value);
+	newLiTitle.appendChild(newTitleText);
+
+	var newLiRating = document.createElement("li");
+	newLiRating.setAttribute("id", "movie2-rating");
+	newLiRating.setAttribute("class", "rating");
+	var newRatingText = document.createTextNode(newRating.value);
+	newLiRating.appendChild(newRatingText);
+
+	var newH3 = document.createElement("h3");
+	newH3.setAttribute("id", "movie2-label");
+	var newH3Text = document.createTextNode('Movie 2');
+	newH3.appendChild(newH3Text);
+
+	//NAppend li --> ul
+	newUl.appendChild(newLiTitle);
+	newUl.appendChild(newLiRating);
+	//Append h3 and ul --> newDiv
+	newDiv.appendChild(newH3);
+	newDiv.appendChild(newUl);
 	
+	console.log(newDiv);
+
+	//Add new div after existing-movie1
+	var existingMovies = document.getElementById('existing-entries');
+	existingMovies.insertBefore(newDiv, existingMovies.lastChild);
+	
+	//Reset form back to blank
+	document.getElementById('new-movie-form').reset();
+
+	//format rating
+	formatRating();
 
 }, false);
