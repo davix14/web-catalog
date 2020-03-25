e675aabc8a98040db1f944d25a698692552b5a93
David <jdavidjuarez95@gmail.com>
2020-03-24 22:02:18 -0400

    Added rating formatting functionality and function to get values from form

--- public/app_Main.js
+++ public/app_Main.js
@@ -1,5 +1,5 @@
 //Take input from form and add to "Existing List"
-function addListItem(){
+function changeColor() {
 	var list;
 	list = document.getElementById('movie1-rating');
 	list.className = 'good';
@@ -8,4 +8,34 @@ function addListItem(){
 
 var link;
 link = document.getElementById('movie1-rating');
-link.addEventListener('click', addListItem, false);
+link.addEventListener('click', changeColor, false);
+
+//Add event to page load for formatting of rating
+window.addEventListener('load', formatRating, false);
+
+function formatRating() {
+	//log events
+	console.log('Page loaded! formatRating running');
+	var rating = document.getElementsByClassName('rating');
+	for (var i = 0; i < rating.length; i++) {
+		var value = parseFloat(rating[i].innerHTML);
+		rating[i].innerHTML = value + "/5";
+		//debug only -> console.log(rating[i]);
+	}
+}
+
+//Add new list if the user clicks submit
+var submitBtn = document.getElementById('new-movie-submit');
+
+submitBtn.addEventListener('click', function(e){
+	e.preventDefault(); //prevent default action (aka submitting form)
+	//get values of form
+	var newTitle = document.getElementById('new-movie-title');
+	var newRaing = document.getElementById('new-movie-rating');
+
+	console.log(newTitle.value);
+	console.log(newRaing.value);
+
+	
+
+}, false);
