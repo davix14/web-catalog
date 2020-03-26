7a0431f5fe4a4505feac17818ce3713dcee36112
David <jdavidjuarez95@gmail.com>
2020-03-26 02:47:48 -0400

    Added jquery listener and function (not working)

--- public/app_Main.js
+++ public/app_Main.js
@@ -25,68 +25,68 @@ function formatRating() {
 }
 
 //Add new list if the user clicks submit button
-var submitBtn = document.getElementById('new-movie-submit');
+// var submitBtn = document.getElementById('new-movie-submit');
 
-submitBtn.addEventListener('click', function (e) {
-	e.preventDefault(); //prevent default action (aka submitting form)
-	//get values of form
-	var newTitle = document.getElementById('new-movie-title');
-	var newRating = document.getElementById('new-movie-rating');
+// submitBtn.addEventListener('click', function (e) {
+// 	e.preventDefault(); //prevent default action (aka submitting form)
+// 	//get values of form
+// 	var newTitle = document.getElementById('new-movie-title');
+// 	var newRating = document.getElementById('new-movie-rating');
 
-	// console.log(newTitle.value);
-	// console.log(newRating.value);
+// 	// console.log(newTitle.value);
+// 	// console.log(newRating.value);
 
-	//create new elements with id's and text content
-	var newTd = document.createElement('td');
-	newTd.setAttribute('id', 'movie2-td');
+// 	//create new elements with id's and text content
+// 	var newTd = document.createElement('td');
+// 	newTd.setAttribute('id', 'movie2-td');
 
-	var newDiv = document.createElement('div');
-	newDiv.setAttribute('id', 'existing-movie2');
-	newDiv.setAttribute('class', 'existing-movie-container');
+// 	var newDiv = document.createElement('div');
+// 	newDiv.setAttribute('id', 'existing-movie2');
+// 	newDiv.setAttribute('class', 'existing-movie-container');
 
-	var newUl = document.createElement('ul');
-	newUl.setAttribute('id', 'movie2-list');
+// 	var newUl = document.createElement('ul');
+// 	newUl.setAttribute('id', 'movie2-list');
 
-	var newLiTitle = document.createElement('li');
-	newLiTitle.setAttribute('id', 'movie2-title');
-	var newTitleText = document.createTextNode(newTitle.value);
-	newLiTitle.appendChild(newTitleText);
+// 	var newLiTitle = document.createElement('li');
+// 	newLiTitle.setAttribute('id', 'movie2-title');
+// 	var newTitleText = document.createTextNode(newTitle.value);
+// 	newLiTitle.appendChild(newTitleText);
 
-	var newLiRating = document.createElement('li');
-	newLiRating.setAttribute('id', 'movie2-rating');
-	newLiRating.setAttribute('class', 'rating');
-	var newRatingText = document.createTextNode(newRating.value);
-	newLiRating.appendChild(newRatingText);
+// 	var newLiRating = document.createElement('li');
+// 	newLiRating.setAttribute('id', 'movie2-rating');
+// 	newLiRating.setAttribute('class', 'rating');
+// 	var newRatingText = document.createTextNode(newRating.value);
+// 	newLiRating.appendChild(newRatingText);
 
-	var newH3 = document.createElement('h3');
-	newH3.setAttribute('id', 'movie2-label');
-	var newH3Text = document.createTextNode('Movie 2');
-	newH3.appendChild(newH3Text);
+// 	var newH3 = document.createElement('h3');
+// 	newH3.setAttribute('id', 'movie2-label');
+// 	var newH3Text = document.createTextNode('Movie 2');
+// 	newH3.appendChild(newH3Text);
 
-	//NAppend li --> ul
-	newUl.appendChild(newLiTitle);
-	newUl.appendChild(newLiRating);
-	//Append h3 and ul --> newDiv
-	newDiv.appendChild(newH3);
-	newDiv.appendChild(newUl);
-	newTd.appendChild(newDiv);
+// 	//NAppend li --> ul
+// 	newUl.appendChild(newLiTitle);
+// 	newUl.appendChild(newLiRating);
+// 	//Append h3 and ul --> newDiv
+// 	newDiv.appendChild(newH3);
+// 	newDiv.appendChild(newUl);
+// 	newTd.appendChild(newDiv);
 
-	//console.log(newTd);
+// 	//console.log(newTd);
 
-	//Add new div after existing-movie1
-	var existingMovies = document.getElementById('em-row1');
-	existingMovies.insertBefore(newTd, existingMovies.lastChild);
+// 	//Add new div after existing-movie1
+// 	var existingMovies = document.getElementById('em-row1');
+// 	existingMovies.insertBefore(newTd, existingMovies.lastChild);
 
-	//Reset form back to blank
-	document.getElementById('new-movie-form').reset();
+// 	//Reset form back to blank
+// 	document.getElementById('new-movie-form').reset();
 
-	//format rating
-	formatRating();
+// 	//format rating
+// 	formatRating();
 
-	var movies = document.getElementsByClassName('existing-movie-container');
-	console.log('No of Movies: ' + movies.length);
+// 	var movies = document.getElementsByClassName('existing-movie-container');
+// 	console.log('No of Movies: ' + movies.length);
 
-}, false);
+// }, false);
 
 //Add eventListener to submit button
 //Prevent default
@@ -98,3 +98,53 @@ submitBtn.addEventListener('click', function (e) {
 //Format rating
 //ALLOW MAX OF 4 td's per row
 
+//Added event listener on submit button
+$('#new-movie-submit').on('click', function(e){
+	e.preventDefault();//Prevents submit
+
+	//Get values from form and count of existing movies for row logic
+	var newTitle = $('#new-movie-title').val();
+	var newRating = $('#new-movie-rating').val();
+	var existingMovieCount = $('.existing-movie-container').length;
+	var newCount = existingMovieCount + 1; // No of current movie entry
+	var rowCount = $('.table-row').length;
+
+	//For debugging
+	console.log(newTitle);
+	console.log(newRating);
+	console.log(existingMovieCount);
+	console.log(rowCount);
+
+	//Reset all form elements
+	$('#new-movie-form')[0].reset();
+
+	//Create new elements with id's/classes
+	var newMovieTd = $('<td>').attr('id', 'movie'+newCount+'-td');
+	var newDiv, newH3, newUl, newLiTitle, newLiRating;
+	newLiRating = $('<li>').attr('id', 'movie'+newCount+'-rating').text(newRating);
+	newLiTitle = $('<li>').attr('id', 'movie'+newCount+'-title').text(newTitle);
+	newUl = $('<ul>').attr('id', 'movie'+newCount+'-list').append(newLiTitle, newLiRating);
+	newH3 = $('<h3>').attr('id', 'movie'+newCount+'-label').text('Movie '+newCount);
+	newDiv = $('<div>').addClass('existing-movie-container').attr('id', 'existing-movie'+newCount).append(newH3, newUl);
+	newMovieTd.append(newDiv);
+
+	//Check if there are 3 or less items in this row
+	//if == 4 row is full! Create new row
+	//if row count.
+	var latestRowCount = $('#em-row'+rowCount).children;
+
+	console.log(latestRowCount);
+
+	if(/* latestRowCount.length */0 <= 3){//add to current row
+		$('#em-row'+rowCount).append(newMovieTd);
+	}else{//create new row and add all elements here
+		var rowNum = rowCount + 1;
+		var newRow = $('<tr>').addClass('table-row').attr('id', 'em-row'+rowNum);
+		newRow.append(newMovieTd);
+
+	}
+
+	formatRating();
+});
+
+//Create
