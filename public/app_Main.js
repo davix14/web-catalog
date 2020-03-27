a3fef9a9aacc141ca2493802b5df18baf5115847
David <jdavidjuarez95@gmail.com>
2020-03-26 21:02:29 -0400

    Cleaned up app.js  to use only new jquery syntax

--- public/app_Main.js
+++ public/app_Main.js
@@ -24,81 +24,8 @@ function formatRating() {
 	}
 }
 
-//Add new list if the user clicks submit button
-// var submitBtn = document.getElementById('new-movie-submit');
-
-// submitBtn.addEventListener('click', function (e) {
-// 	e.preventDefault(); //prevent default action (aka submitting form)
-// 	//get values of form
-// 	var newTitle = document.getElementById('new-movie-title');
-// 	var newRating = document.getElementById('new-movie-rating');
-
-// 	// console.log(newTitle.value);
-// 	// console.log(newRating.value);
-
-// 	//create new elements with id's and text content
-// 	var newTd = document.createElement('td');
-// 	newTd.setAttribute('id', 'movie2-td');
-
-// 	var newDiv = document.createElement('div');
-// 	newDiv.setAttribute('id', 'existing-movie2');
-// 	newDiv.setAttribute('class', 'existing-movie-container');
-
-// 	var newUl = document.createElement('ul');
-// 	newUl.setAttribute('id', 'movie2-list');
-
-// 	var newLiTitle = document.createElement('li');
-// 	newLiTitle.setAttribute('id', 'movie2-title');
-// 	var newTitleText = document.createTextNode(newTitle.value);
-// 	newLiTitle.appendChild(newTitleText);
-
-// 	var newLiRating = document.createElement('li');
-// 	newLiRating.setAttribute('id', 'movie2-rating');
-// 	newLiRating.setAttribute('class', 'rating');
-// 	var newRatingText = document.createTextNode(newRating.value);
-// 	newLiRating.appendChild(newRatingText);
-
-// 	var newH3 = document.createElement('h3');
-// 	newH3.setAttribute('id', 'movie2-label');
-// 	var newH3Text = document.createTextNode('Movie 2');
-// 	newH3.appendChild(newH3Text);
-
-// 	//NAppend li --> ul
-// 	newUl.appendChild(newLiTitle);
-// 	newUl.appendChild(newLiRating);
-// 	//Append h3 and ul --> newDiv
-// 	newDiv.appendChild(newH3);
-// 	newDiv.appendChild(newUl);
-// 	newTd.appendChild(newDiv);
-
-// 	//console.log(newTd);
-
-// 	//Add new div after existing-movie1
-// 	var existingMovies = document.getElementById('em-row1');
-// 	existingMovies.insertBefore(newTd, existingMovies.lastChild);
-
-// 	//Reset form back to blank
-// 	document.getElementById('new-movie-form').reset();
-
-// 	//format rating
-// 	formatRating();
-
-// 	var movies = document.getElementsByClassName('existing-movie-container');
-// 	console.log('No of Movies: ' + movies.length);
-
-// }, false);
-
-//Add eventListener to submit button
-//Prevent default
-//Get form values
-//Create elements with id's
-//Nest elements
-//Insert into document
-//Clear form
-//Format rating
-//ALLOW MAX OF 4 td's per row
-
-//Added event listener on submit button
+//Added event listener on submit button to take form input and
+//add new td and/or row to table of existing movies
 $('#new-movie-submit').on('click', function(e){
 	e.preventDefault();//Prevents submit
 
