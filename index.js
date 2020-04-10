a210f918fa89da5e52390c04cfca47136e2e8182
David Juarez <jdavidjuarez95@gmail.com>
2020-04-10 11:17:39 -0400

    Cleaned up index.js file

--- index.js
+++ index.js
@@ -56,19 +56,6 @@ app.post('/sendMovies', function (req, res) {//if post received
 
 });
 
-// //function to handle post request
-// app.post('/sendMovies', function (req, res) {
-//     console.log('post received');
-//     // var obj = JSON.parse(req.body);
-//     var title = req.body.title;
-//     var rating = req.body.rating;
-//     console.log("Movie received:" + title + " Rating received:" + rating);
-//     // res.sendStatus(200);
-//     res.set('Content-Type', 'application/JSON');
-//     res.status(200).send(JSON.stringify({ saved: true }));
-
-// });
-
 //Start web server on port 3010
 app.listen(3010);
 console.log('Server running and listening');
